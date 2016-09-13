a77dcc40a608a88359f0fdb0a82dfef5b933df21
gewethor@github.com <gwethor@gmail.com>
2016-09-12 18:48:04 -0700

    end of task 14

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,18 @@ import Ember from "ember";
 var Photo = Ember.Object.extend({
     title: '',
     username: '',
-    url: '',
+    //flickr extra data
+    owner: '',
+    //flickr url data
+    id: '',
+    farm: 0,
+    secret:'',
+    server: '',
+    url: function() {
+        return "https://farm"+this.get('farm')+
+        ".staticflickr.com/"+this.get('server')+
+        "/"+this.get('id')+"_"+this.get('secret')+"_b.jpg";
+        }.property('farm', 'server', 'id', 'secret'),
    });
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
@@ -41,7 +52,7 @@ testPhotos.pushObject(testimg1);
 testPhotos.pushObject(testimg2);
 testPhotos.pushObject(testimg3);
 testPhotos.pushObject(testimg4);
-
+/*
 export default Ember.Controller.extend({
     photos: testPhotos,
     searchField: '',
@@ -59,4 +70,51 @@ export default Ember.Controller.extend({
            );
         }
     }
+});
+*/
+export default Ember.Controller.extend({
+    photos: testPhotos,
+    searchfield: '',
+    filteredPhotos: function () {
+        var filter = this.get('searchField');
+        var rx = new RegExp(filter,'gi');
+        var photos = this.get('photos');
+
+        return photos.filter(function(photo){
+            return photo.get('title').match(rx) || photo.get('username').match(rx);
+        });
+    }.property('photos.@each', 'seachField'),
+    actions: {
+        search: function() {
+            this.get('filteredPhotos');
+        },
+
+    getPhotos: function() {
+    var apiKey = 'a06516fcecef0e96833dc70617e54917';
+    var host = 'https://api.flickr.com/services/rest/';
+    var method = "flickr.tags.getClusterPhotos";
+    var tag = "glitter";
+    var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tag="+tag+"&format=json&nojsoncallback=1";
+    var photos = this.get('photos');
+     Ember.$.getJSON(requestURL, function(data){
+    //callback for successfully completed requests
+        console.log(data);
+        data.photos.photo.map(function(photo) {
+            var newPhotoItem = Photo.create({
+                title: photo.title,
+                username: photo.username,
+                //flickr extra data
+                owners: photo.owner,
+                //flickr url data
+                id: photo.id,
+                farm: photo.farm,
+                secret: photo.secret,
+                server: photo.server,
+            });
+            photos.pushObjects(newPhotoItem);
+
+            });
+        });
+    },
+}
 });
